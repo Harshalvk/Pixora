fefb7825efdbbe07d45ecc8807eb411b52d77737
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-21 15:12:31 +0530

    refactor: remove global and page-specific CSS, integrate Tailwind CSS for styling

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      Pixora
+    <div className="">
+      <div className="text-2xl bg-gradient-to-r from-blue-400 to-blue-800 text-white border border-blue-950 container mx-auto flex items-center justify-center rounded-md my-5 font-bold tracking-tight">
+        Pixora
+      </div>
+      <div>
+        <p className="">hello</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
