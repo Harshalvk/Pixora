4a109f4035c06d1c5b4c6f08d47d1d8f3fdaa551
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-23 17:43:56 +0530

    feat(frontend): initialize worker frontend with Next.js, Tailwind CSS, and essential configurations

---
+++ apps/worker-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-teal-700 text-2xl">Worker Frontend</h1>
+    </div>
+  );
+}
