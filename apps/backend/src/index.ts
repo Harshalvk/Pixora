0aca15445ebf9c99c0b173f5c1a8b61d4958145b
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-25 22:26:48 +0530

    feat(backend): configure dotenv and add user routes to the Express app

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 const PORT = 5050;
+
+app.use("/v1/user", userRouter);
+// app.use("/v1/worker", workerRouter)
 
 app.get("/health", (req, res) => {
   res.send("Server health is OK!");
