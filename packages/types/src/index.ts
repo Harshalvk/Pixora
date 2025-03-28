e26b2670373f1e39343dbb3dbb1d03162dde5738
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-29 01:05:51 +0530

    feat(types): add submission input schema for task submissions

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -10,4 +10,10 @@ export const createTaskInputSchema = z.object({
   signature: z.string(),
 });
 
+export const createSubmissonInputSchema = z.object({
+  taskId: z.string(),
+  selection: z.string(),
+});
+
 export type User = z.infer<typeof createTaskInputSchema>;
+export type SubmissionInput = z.infer<typeof createSubmissonInputSchema>;
